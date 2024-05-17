b10bac08541a923a7594236e400d2103ea1f581e
waqas8raza <rajputwaqass887@gmail.com>
2024-05-17 10:19:56 +0500

    admin panel added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
-import 'package:yoga_app/pages/splash_page.dart';
+import 'package:yoga_app/admin/add_course.dart';
+import 'package:yoga_app/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -19,7 +23,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const SplashPage(),
+        home: AddExerciseScreen(),
       ),
     );
   }
